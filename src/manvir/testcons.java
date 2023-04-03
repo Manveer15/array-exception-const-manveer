7ee6bd897d073c377980fcb57e8a974343d0cdef
Manveer15 <129711912+Manveer15@users.noreply.github.com>
2023-04-02 21:51:54 -0700

    concstruc

---
+++ src/manvir/testcons.java
@@ -0,0 +1,20 @@
+package arrayandexcep;
+
+public class testcons {
+	public construction()
+	{
+	
+	System.out.println("hifrom everybody");
+	}
+	public construction(int a,int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+
+	}
+}
+public class test {
+	
+}
+
+	
