75f4cabcfc2e698940368f3f7790620ebed24857
Manveer15 <129711912+Manveer15@users.noreply.github.com>
2023-04-02 20:47:29 -0700

    Code for array and exception

---
+++ src/manvir/manveer.java
@@ -0,0 +1,21 @@
+package manvir;
+
+import java.util.Scanner;
+
+public class manveer {
+
+
+public static void main (String[] args ) {
+	Scanner scanner=new Scanner(System.in);
+	int a;
+	System.out.println("please input your value");
+	a=scanner.nextInt();
+	if(a/2==0);
+	System.out.println("value is even number");
+	else
+    
+	System.out.println(a);
+
+	
+}
+}
